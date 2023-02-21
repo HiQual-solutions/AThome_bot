664dd558bdf725030656b8ed42febabb25dfbaf5
Iosif <thetawerchannel@gmail.com>
2023-02-21 23:54:10 +0300

    commit

--- src/main.py
+++ src/main.py
@@ -25,15 +25,15 @@ class InvoiceStates(StatesGroup):
 
 from typer import Typer
 
-# from src.db.mongo import db_collection
-# from src.tasks import get_all_dramatiq
+from src.db.mongo import db_collection
+from src.tasks import get_all_dramatiq
 
 # TODO: сделать возможность отправки кнопки закончить после каждого фото
 # TODO: реализовать отложенные задачи
 # TODO: добавить все чаты в .env
 
-# User = db_collection("User")
-# Data_menu = db_collection("Data_menu")
+User = db_collection("User")
+Data_menu = db_collection("Data_menu")
 mybot = Typer()
 logging.basicConfig(level=logging.INFO)
 
@@ -47,20 +47,20 @@ async def welcome(message: types.Message):
     if message.from_user.id != message.chat.id:
         return
 
-    # data = Data_menu.find_by _sort([("period", -1)])
-    # await message.answer(f"Добрый день, {message.from_user.full_name}" + 
-    # f"\n{data['date'][1]}.{data['date'][0]}" + 
-    # f"\nТемпература: {data['temp']} | Влажность: {data['humidity']}%" +
-    # f"\nДавление: {data['pressure']} рт. ст." +
-    # '''f"\nКурс: ${data['currency'][0]}, €{data['currency'][1]}"''', reply_markup=inlineKeyboard)
-
-    data = get_info.get_weather_and_currency()
-    await message.answer(f"Добрый день, {message.from_user.full_name}!", reply_markup=webapp_keyboard)
-    await bot.send_message(message.chat.id, 
+    data = Data_menu.find_by_sort([("period", -1)])
+    await message.answer(f"Добрый день, {message.from_user.full_name}" + 
     f"\n{data['date'][1]}.{data['date'][0]}" + 
     f"\nТемпература: {data['temp']} | Влажность: {data['humidity']}%" +
     f"\nДавление: {data['pressure']} рт. ст." +
-    f"\nКурс: ${data['currency'][0]}, €{data['currency'][1]}", reply_markup=main_keyboard)
+    '''f"\nКурс: ${data['currency'][0]}, €{data['currency'][1]}"''', reply_markup=main_keyboard)
+
+    # data = get_info.get_weather_and_currency()
+    # await message.answer(f"Добрый день, {message.from_user.full_name}!", reply_markup=webapp_keyboard)
+    # await bot.send_message(message.chat.id, 
+    # f"\n{data['date'][1]}.{data['date'][0]}" + 
+    # f"\nТемпература: {data['temp']} | Влажность: {data['humidity']}%" +
+    # f"\nДавление: {data['pressure']} рт. ст." +
+    # f"\nКурс: ${data['currency'][0]}, €{data['currency'][1]}", reply_markup=main_keyboard)
     # await bot.send_message(message.chat.id,"-",reply_markup=main_keyboard)
 
 
@@ -125,5 +125,5 @@ def run() -> None:
     
     logging.info("[RUN SERVICE]")
     
-    # get_all_dramatiq()
+    get_all_dramatiq()
     executor.start_polling(dp, skip_updates=False)
